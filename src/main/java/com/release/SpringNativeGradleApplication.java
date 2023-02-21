ab09f6620887bb0eeee4bfc35f5a9f781b5820e8
延成 <cyc.release@gmail.com>
2023-02-21 17:10:51 +0800

    Initial commit

---
+++ src/main/java/com/release/SpringNativeGradleApplication.java
@@ -0,0 +1,13 @@
+package com.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeGradleApplication.class, args);
+    }
+
+}
